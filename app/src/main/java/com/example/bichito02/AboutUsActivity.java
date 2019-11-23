907698a56f71e8d52a133c43a445555f039b27a2
unknown <Admin@IHR80PBF211.continental.edu.pe>
2019-11-23 13:03:18 -0500

    primer cambio

---
+++ app/src/main/java/com/example/bichito02/AboutUsActivity.java
@@ -0,0 +1,16 @@
+package com.example.bichito02;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AboutUsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about_us);
+
+
+    }
+}
