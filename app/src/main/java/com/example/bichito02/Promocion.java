42db4da3026e8bc433f9e90bbbb7c68f92736aba
unknown <Admin@IHR80PBF191.continental.edu.pe>
2019-12-04 21:20:37 -0500

    :D

---
+++ app/src/main/java/com/example/bichito02/Promocion.java
@@ -0,0 +1,13 @@
+package com.example.bichito02;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Promocion extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_promocion);
+    }
+}
